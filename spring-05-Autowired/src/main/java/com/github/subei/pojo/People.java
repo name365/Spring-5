c48a9dcb65c07bfdbd66b09f27d4e676548047cf
Mr. Subei <48648114+name365@users.noreply.github.com>
2021-01-25 10:25:17 +0800

    java

--- spring-05-Autowired/src/main/java/com/github/subei/pojo/People.java
+++ spring-05-Autowired/src/main/java/com/github/subei/pojo/People.java
@@ -1,7 +1,11 @@
 package com.github.subei.pojo;
 
+import javax.annotation.Resource;
+
 public class People {
+    @Resource(name="cat2")
     private Cat cat;
+    @Resource
     private Dog dog;
     private String name;
 
@@ -9,24 +13,12 @@ public class People {
         return cat;
     }
 
-    public void setCat(Cat cat) {
-        this.cat = cat;
-    }
-
     public Dog getDog() {
         return dog;
     }
 
-    public void setDog(Dog dog) {
-        this.dog = dog;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
