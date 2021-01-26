89d7686df27b23abc7f8f0cc89aac56c55138d8e
Mr. Subei <48648114+name365@users.noreply.github.com>
2021-01-26 19:12:00 +0800

    java

--- spring-09-AOP/src/main/java/com/github/subei/diy/AnnotationPointCut.java
+++ spring-09-AOP/src/main/java/com/github/subei/diy/AnnotationPointCut.java
@@ -1,6 +1,7 @@
 package com.github.subei.diy;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -19,15 +20,17 @@ public class AnnotationPointCut {
         System.out.println("---------方法执行后---------");
     }
 
+    // 在环绕增强中，我们可以给定一个参数，代表我们要获取处理切入的点
     @Around("execution(* com.github.subei.service.UserServiceImpl.*(..))")
     public void around(ProceedingJoinPoint jp) throws Throwable {
         System.out.println("环绕前");
-        System.out.println("签名:"+jp.getSignature()); // 获得签名
+        Signature signature = jp.getSignature();
+        System.out.println("签名:"+ signature); // 获得签名
 
         // 执行目标方法:proceed
         Object proceed = jp.proceed();
-
         System.out.println("环绕后");
+
         System.out.println(proceed);
     }
 }
